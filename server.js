8b22583822bfb415fa8b027715c4f4f423d36d6d
ysaad01 <ysaad2325@gmail.com>
2022-04-30 18:32:47 -0400

    created mysql connection in server.js file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,23 @@ const app = express();
 /* Express middleware */
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+/* connect to database */
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your mySQL username
+    user: 'root',
+    // Your MySQL
+    password: 'Sohamaged',
+    database: 'election'
+  },
+  console.log("Connected to the election database.")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 /* Default response for any other request (Not Found) */
 app.use((req, res) => {
