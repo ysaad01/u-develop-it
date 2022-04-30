e9999b14d49d2b5fce2ace24830c76b46bff0df8
ysaad01 <ysaad2325@gmail.com>
2022-04-30 18:21:11 -0400

    added server.js file and established express connection

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+/* Express middleware */
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+/* Default response for any other request (Not Found) */
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+/* start the Express.js server on port 3001 */
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
