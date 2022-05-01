2477da16161ffb8c8929e08ec6556b1ce1209051
ysaad01 <ysaad2325@gmail.com>
2022-04-30 20:51:28 -0400

    joined candidates and parties tables

--- server.js
+++ server.js
@@ -23,8 +23,12 @@ const db = mysql.createConnection(
 );
 
 /* Get all candidates */
-app.get('/api/candidates', (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+app.get("/api/candidates", (req, res) => {
+  const sql = `SELECT candidates.*, parties.name 
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -32,15 +36,20 @@ app.get('/api/candidates', (req, res) => {
       return;
     }
     res.json({
-      message: 'success',
-      data: rows
+      message: "success",
+      data: rows,
     });
   });
 });
 
 /* Get a single candidate */
 app.get("/api/candidate/:id", (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id
+                WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
