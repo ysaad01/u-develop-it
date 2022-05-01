76c48fa859c8986dfaee2bcbd58fb8dfbd4d360f
ysaad01 <ysaad2325@gmail.com>
2022-05-01 19:41:26 -0400

    downloaded dotenv dependency and updated connection.js file and added file to .gitignore

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,14 @@
+require("dotenv").config();
+
 const mysql = require("mysql2");
 
-/* connect to database */
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // Your mySQL username
-    user: 'root',
-    // Your MySQL
-    password: 'Sohamaged',
-    database: 'election'
-  },
-  console.log("Connected to the election database.")
-);
+const db = mysql.createConnection({
+  host: "localhost",
+  // Your MySQL username,
+  user: process.env.DB_USER,
+  // Your MySQL password
+  password: process.env.DB_PW,
+  database: process.env.DB_NAME,
+});
 
 module.exports = db;
